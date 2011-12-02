206b846e72455c041ada1582f2c85910776f8c49
ddkang <ddkang@mit.edu>
2011-12-02 11:30:19 -0500

    Daniel: Initial code and build properties.

--- daniel-calendar/src/test/scala/TestCalendar.scala
+++ daniel-calendar/src/test/scala/TestCalendar.scala
@@ -5,12 +5,20 @@ import org.scalatest.Assertions
 
 import cap.scalasmt.webapp._
 
+import java.util._
+
 class TestCalendar extends FunSuite {
 
-  test ("dummyVal") {
+/*  test ("dummyVal") {
     val c: Calendar = new Calendar();
 
     expect("hello") { c.getDummyVal(); }
-  }
+  }*/
 
+	test("Event toString") {
+		val d:Date = new Date(1322503200);
+		val e:Event = new Event("UROP Meeting",d,"Group meeting","Stata Center");
+
+		expect("dummyVal") { e.toString(); }
+	}
 }
