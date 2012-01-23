011b02e468f652fa107c75f68b07aa4ccf3b7282
jean yang writeme <jean.yang.writeme@gmail.com>
2012-01-23 13:17:14 -0500

    updates

--- jconf/src/main/scala/ConfUser.scala
+++ jconf/src/main/scala/ConfUser.scala
@@ -30,10 +30,14 @@ trait UserID {
 case class Username (name: String) extends JeevesRecord
 case class Name (name: String) extends JeevesRecord
 case class Password (val pwd: String) extends JeevesRecord
-case class ConfUser( val username: Username, val name: Name, _password: String
+case class ConfUser(
+    val username: Username
+  , val name: Name
+  , private var _password: String
   , val role: UserStatus )
   extends JeevesRecord {
-    val password: Symbolic = {
+    def setPassword(newpwd: String):
+    val getPassword(): Symbolic = {
       val level = mkLevel ();
       policy (level, !(CONTEXT.viewer === this), LOW);
       mkSensitive(level, Password(_password), Password("default"))
