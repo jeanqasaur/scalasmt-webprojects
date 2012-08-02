eef4e8795c2852fc27e4c8d801ba4d8167d5612d
Leandro Doctors <leandro.doctors@cs.kuleuven.be>
2012-08-02 22:04:50 +0200

    Modify: add User Entries (Posts)

--- socialnetstar/src/main/scala/cap/jeeves/socialnetstar/backend/UserRecord.scala
+++ socialnetstar/src/main/scala/cap/jeeves/socialnetstar/backend/UserRecord.scala
@@ -15,6 +15,8 @@ case class Name(s: String) extends JeevesRecord
 case class Email(s: String) extends JeevesRecord
 case class SocialCircle(s: String) extends JeevesRecord
 
+case class Entry(e:String, l:PrivacyLevel) extends JeevesRecord
+
 sealed trait PrivacyLevel 
 object Anyone extends PrivacyLevel
 object Self extends PrivacyLevel
@@ -31,21 +33,35 @@ class UserRecord(
   emailL: PrivacyLevel, 
   circleV: SocialCircle, 
   circleL: PrivacyLevel, 
-  friendsL: PrivacyLevel) 
+  friendsL: PrivacyLevel,
+  entriesV:Set[Entry]=Set()
+  entriesL:PrivacyLevel)
+  
 extends JeevesRecord {
   private var friends: Set[UserRecord] = Set()
   var X: IntExpr = 1000
   var Y: IntExpr = 1000
 
   /** Mutators */
-  def add(u: UserRecord) {friends = friends + u}
-  def remove(u: UserRecord) {friends = friends - u}
+  def addFriend(u: UserRecord) {friends = friends + u}
+  def removeFriend(u: UserRecord) {friends = friends - u}
   def setLocation(x: BigInt, y: BigInt)(implicit ctxt:Sensitive){
     val l = mkLevel();
     restrict(l, distance(ctxt, this) >= 10);
     UserRecord.this.X = mkSensitiveInt(l, x, 1000);
     UserRecord.this.Y = mkSensitiveInt(l, y, 1000);
   }
+  
+  def addEntry(e:Entry) {entries=entries+e}
+  
+  def removeEntry(e:Entry) {entries=entries-e}
+
+  def shareEntryWith(e:Entry, u:UserRecord) (implicit ctxt:Sensitive){
+    // add e to u's entries set
+    
+    val l=level(u.entriesL)
+    restrict(l,u.addEntry(e,this))
+  }	
 
  /** Observers */
   val name = mkSensitive(level(nameL), nameV)
