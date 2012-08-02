1593549a0d33e27a4208344a40a356164fea6b3e
Leandro Doctors <leandro.doctors@cs.kuleuven.be>
2012-08-02 21:30:51 +0200

    Modify: Renamed many (case) classes and traits: - SocialNetBackend -->SNSBackend, - SocialContext --> SNSContext, - UserLevel --> PrivacyLevel, - Network --> SocialCircle

--- socialnetstar/src/main/scala/cap/jeeves/socialnetstar/backend/SNSContext.scala
+++ socialnetstar/src/main/scala/cap/jeeves/socialnetstar/backend/SNSContext.scala
@@ -5,5 +5,5 @@ package cap.jeeves.socialnetstar.backend
 
 import cap.jeeves.socialnetstar.backend.SocialNetBackend._
 
-case class SocialContext (viewer:UserRecord) extends JeevesRecord
+case class SNSContext (viewer:UserRecord) extends JeevesRecord
 
