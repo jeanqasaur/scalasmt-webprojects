2fa72e836f8bbb987d9e14e68a4c448cedb0b122
Leandro Doctors <leandro.doctors@cs.kuleuven.be>
2012-08-03 22:48:36 +0200

    SocialNetStar: Add JeevesLib sources, so NetBeans' "Navigate --> Go to Declaration" can work. (A better approach would be to incorporate SocialNetStar sources into JeevesLib project (after JeevesLib sources are rearranged to respect its own hierarchy, as I did with ScalaSMT)

---
+++ socialnetstar/src/test/scala/cap/jeeves/TestJeevesLib.scala
@@ -0,0 +1,56 @@
+package test.cap.jeeves
+
+import cap.scalasmt._
+import cap.jeeves._
+import org.scalatest.FunSuite
+import org.scalatest.Assertions.{expect}
+import scala.collection.immutable.Map
+import cap.jeeves.JeevesTypes._
+
+class ExampleJeevesLib extends FunSuite with JeevesLib {
+  test ("sensitive int") {
+    val l = mkLevel();
+    val x = mkSensitiveInt(l, 42, -1);
+
+    expect(42) {concretize(l === HIGH, x)};
+    expect(-1) {concretize(l === LOW, x)};
+  }
+
+  test ("sensitive object") {
+    val l = mkLevel();
+    val t = Test(1);
+
+    val x = mkSensitive(l, t, NULL);
+    
+    expect(t) {concretize(l === HIGH, x)};
+    expect(null) {concretize(l === LOW, x)};
+  }
+
+  case class Test(id: Int) extends JeevesRecord
+  test ("test restrict") {
+    val x = Test(1)
+    val a = mkLevel ()
+    restrict (a, (ctxt: Sensitive) => ctxt === x)
+    val xS = mkSensitive (a, x, Test(-1))
+    expect (x) { concretize (x, xS) }
+  }
+
+  /*
+  test ("concretizeList non-null") {
+    val x = pickObject[Test];
+    assume(x === Test(0));
+    val symList = List(x);
+    val cList : List[Test] = concretize(NULL, symList);
+    expect(List(Test(0))) {cList};
+  }
+  */
+
+  /*
+  test ("concretizeList null") {
+    val x = pickObject[Test](NULL);
+    val symList = List(x);
+    val cList : List[Test] = concretize(NULL, symList);
+    expect(Nil) {cList};
+  }
+  */
+}
