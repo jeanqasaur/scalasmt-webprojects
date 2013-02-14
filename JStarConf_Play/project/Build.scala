3b8d2ab09aa44a510648289bba39b6e93d8233c5
amadu23 <amadu23@gmail.com>
2013-02-13 21:48:28 -0500

    error in play setup

--- JStarConf_Play/project/Build.scala
+++ JStarConf_Play/project/Build.scala
@@ -7,12 +7,16 @@ object ApplicationBuild extends Build {
     val appName         = "JStarConf"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
+   // From the Play tutorial
+   val appDependencies = Seq(
+     // Add your project dependencies here,
+     "org.squeryl" %% "squeryl" % "0.9.5-2", // Copied from above so that it compiles (?)
+     "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+      
     )
 
 }
