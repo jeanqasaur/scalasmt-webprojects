895f805eae92f893262bdeacf254775b6990e1f7
amadu23 <amadu23@gmail.com>
2013-03-04 10:15:59 -0500

    updates to the login page

--- JStarConfPlay/app/controllers/Application.scala
+++ JStarConfPlay/app/controllers/Application.scala
@@ -2,11 +2,59 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
+
+  val loginForm = Form(
+      tuple (
+      "username" -> nonEmptyText,
+      "password" -> nonEmptyText
+      ))
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.login_user(loginForm))
   }
-  
+
+  def about = TODO
+
+  def sendPassword = TODO
+
+  def loginUser = TODO
+
+  def login = TODO
+
+  def validateProfile = TODO
+
+  def updateProfile = TODO
+
+  def profile = TODO
+
+  def postPaper = TODO
+
+  def getPaper = TODO
+
+  def uploadedPaper = TODO
+
+  def newPaper = TODO
+
+  def editPaper = TODO
+
+  def withdrawPaper = TODO
+
+  def assignPapers = TODO
+
+  def paperAssignments = TODO
+
+  def makeReview = TODO
+
+  def getReview = TODO
+
+  def editReview = TODO
+
+  def editProfile = TODO
+
+  def logout = TODO  
+
 }
